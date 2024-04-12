1ca803765140c54bd15dd6debefe1d5e059e4945
rupomhasan <rupom.hasan607299@gmail.com>
2024-04-12 08:58:22 +0600

    complate the enviroment setup also implement some features on the home page like banner , category section and so one

--- src/Pages/Home/Chef-Service/ChefService.jsx
+++ src/Pages/Home/Chef-Service/ChefService.jsx
@@ -1,11 +1,13 @@
 import img from "../../../assets/home/chef-service.jpg";
 const ChefService = () => {
   return (
-    <div className="my-20 mx-2">
-      <div className="relative">
-        <img src={img} alt="ChefService" />
-        <div className="flex justify-center bg-white max-w-md ">
-          <div className="absolute top-1/2 bottom-1/2 ">
+    <div className=" mx-2 bg-white">
+      <div className="relative ">
+        <div className="bg-gray-200">
+          <img src={img} alt="ChefService " />
+        </div>
+        <div className="absolute inset-0 flex justify-center items-center  ">
+          <div className="max-w-xl bg-white  p-4">
             <h3 className="text-4xl font-thin my-2  text-center">
               Bistro Boss
             </h3>
